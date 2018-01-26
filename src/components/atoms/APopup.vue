635243c28e96417f66e047024fd854d43e345509
Michael Scharl <michael.scharl@me.com>
2018-01-26 15:46:32 +0100

    Added a generic popup component

---
+++ src/components/atoms/APopup.vue
@@ -0,0 +1,81 @@
+<template>
+    <transition name="a-popup" appear>
+        <div class="a-popup__container">
+            <aside class="a-popup__backdrop"></aside>
+            <div class="a-popup__wrapper">
+                <div class="a-popup">
+                    <header class="a-popup__header">
+                        <h1 class="a-popup__title" v-if="title">{{ title }}</h1>
+                        <a-icon-button tag="i" icon="close" class="a-popup__close" @click="close"></a-icon-button>
+                    </header>
+                    <section class="a-popup__content">
+                        <slot></slot>
+                    </section>
+                    <footer class="a-popup__footer">
+                        <slot name="footer"></slot>
+                    </footer>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+    import AIconButton from './AIconButton.vue';
+    import * as KeyCode from '../../constants/KeyCode';
+
+    export default {
+        name      : 'APopup',
+        components: {
+            AIconButton,
+        },
+
+        props: {
+            title: String,
+        },
+
+        data() {
+            return {
+                keyupListener: null,
+                hide         : false,
+            }
+        },
+
+        /**
+         * Start up the component
+         */
+        mounted() {
+            //
+            this.keyupListener = (event) => this.onKeyup(event);
+            document.addEventListener('keyup', this.keyupListener);
+        },
+
+        /**
+         * Handle things before component gets destroyed
+         */
+        beforeDestroy() {
+            // Remove event listener to prevent unused listeners
+            document.removeEventListener('keyup', this.keyupListener);
+        },
+
+        methods: {
+            /**
+             * Emits the close event
+             * */
+            close() {
+                this.$emit('close')
+            },
+
+            /**
+             * Event handler for a key-up-event
+             * @param event
+             */
+            onKeyup(event) {
+                // call close method if ESC key is pressed
+                if(event.keyCode === KeyCode.ESC) {
+                    this.close();
+                }
+            },
+        },
+    }
+</script>
---
+++ src/constants/KeyCode.ts
@@ -0,0 +1,9 @@
+type KeyCode = number
+
+export const ESC: KeyCode = 27;
+export const ENTER: KeyCode = 13;
+
+export const ARROW_LEFT: KeyCode = 37;
+export const ARROW_UP: KeyCode = 38;
+export const ARROW_RIGHT: KeyCode = 39;
+export const ARROW_DOWN: KeyCode = 40;
