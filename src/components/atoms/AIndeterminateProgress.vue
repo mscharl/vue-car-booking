1d1ad7539a4f6eefb0c9a2c0977f0553e910acad
Michael Scharl <michael.scharl@me.com>
2018-02-07 08:36:29 +0100

    Added an indeterminate progress component

---
+++ src/components/atoms/AIndeterminateProgress.vue
@@ -0,0 +1,15 @@
+<script>
+    export default {
+        name: 'AIndeterminateProgress',
+
+        functional: true,
+
+        render(createElement, context) {
+            return createElement('div', {
+                'class': [
+                    context.data.class, context.data.staticClass, 'a-indeterminate-progress',
+                ],
+            });
+        },
+    }
+</script>
